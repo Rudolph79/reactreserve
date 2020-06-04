610cee4a75a56415f08c0572f2665b74c190eba7
EBANG MEZUI ME MVE <rudolph.ebang@gmail.com>
2020-06-04 12:23:39 +0200

    Link in production added

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
     process.env.NODE_ENV === "production"
-        ? 'https://emarh-react-reserve.now.sh'
+        ? 'https://emarh-e-commerce-react-reserve.herokuapp.com'
         : 'http://localhost:3000';
 
 export default baseUrl;
